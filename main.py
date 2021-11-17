c6ac90c91cbc68a242f11b6f12de2c569bed41a4
Josh A <john@localhost.localdomain>
2021-11-17 13:38:01 +0100

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,29 @@ def print_grid(grid):
             print(grid[i][j], end=" ")
         print()
 
+def findEmptyCell(grid, i=0,j=0):
+    for num in range(81):
+        row = num // 9
+        col = num % 9
+        print(row,col)
+        if grid[row][col] == 0:
+            return row,col
+    return True
+
+def checkElem(grid, x, y):
+    elem = grid[x][y]
+    if i < 3 and j < 3:
+        #check all posiblities
+        pass
+
+def solveSudoku(grid):
+    if findEmptyCell(grid):
+        return True
+    else:
+        i, j = findEmptyCell(grid)
+        for elem in range(1,10):
+            print('test')
+
 test_grid = [[2, 5, 0, 0, 3, 0, 9, 0, 1],
             [0, 1, 0, 0, 0, 4, 0, 0, 0],
             [4, 0, 7, 0, 0, 0, 2, 0, 8],
@@ -14,4 +37,5 @@ test_grid = [[2, 5, 0, 0, 3, 0, 9, 0, 1],
             [0, 4, 0, 0, 0, 3, 0, 0, 0],
             [0, 0, 0, 3, 6, 0, 0, 7, 2],
             [0, 7, 0, 0, 0, 0, 0, 0, 3],
-            [9, 0, 3, 0, 0, 0, 6, 0, 4]]
+            [9, 0, 3, 0, 0, 0, 6, 0, 4]]    
+
